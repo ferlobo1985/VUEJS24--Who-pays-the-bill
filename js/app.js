ce974463004717e7b5acdf4003412c492894c440
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-12 17:38:27 -0300

    03-Adding names to the list
    03-Adding names to the list

--- js/app.js
+++ js/app.js
@@ -15,13 +15,26 @@ createApp({
 
         /// methods
         const addNameToList = () => {
-            
+            const userName = data.inputName;
+
+            data.names.push(userName);
+            data.inputName = '';
+
+            console.log(data.names)
+
+        }
+
+
+        const removeName = (index) => {
+            data.names.splice(index,1)
         }
 
 
 
         return {
-            data
+            data,
+            addNameToList,
+            removeName
         }
     }
 }).mount('#app')
